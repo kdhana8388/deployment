ee109cfceb1f0c0b1889f13516e8af3a9a7991f2
dhana <38982655+kdhana8388@users.noreply.github.com>
2019-01-28 16:03:35 +0000

    Adding bat file

--- .muleci/deployment1.bat
+++ .muleci/deployment1.bat
@@ -1,6 +1,7 @@
 echo "=== Deployment has started! ==="
 
 ::check input arguments
-
+IF /I "%%" EQU "0" 
+echo "ERROR: Please, pass the filename for deployment descritor as an argument!"
 
 echo "=== Deployment has finished successfully! ==="
